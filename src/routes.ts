fa7ee8f1c4cc1751c885179bb70ae329b4871edf
herminioneto <herminio_neto@outlook.com>
2024-12-29 17:21:13 -0300

    API em fastify auto-documentável com swagger

---
+++ src/routes.ts
@@ -0,0 +1,63 @@
+import z from "zod";
+import { FastifyTypedInstance } from "./types";
+import { randomUUID } from "node:crypto";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const users: User[] = [];
+
+export async function routes(app: FastifyTypedInstance) {
+  app.get(
+    "/users",
+    {
+      schema: {
+        description: "List users.",
+        tags: ["users"],
+        response: {
+          200: z.array(
+            z.object({
+              id: z.string(),
+              name: z.string(),
+              email: z.string(),
+            })
+          ),
+        },
+      },
+    },
+    () => {
+      return users;
+    }
+  );
+
+  app.post(
+    "/users",
+    {
+      schema: {
+        description: "Create a new user.",
+        tags: ["users"],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null().describe("User created."),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body;
+
+      users.push({
+        id: randomUUID(),
+        name,
+        email,
+      });
+
+      return reply.status(201).send();
+    }
+  );
+}
